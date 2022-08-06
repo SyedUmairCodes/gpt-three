338363d4f9780f3cc5a32ef061245e32e0a02c25
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-08-06 20:03:44 +0500

    Header and brands section completed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something Amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam labore
+          a doloremque saepe unde magni molestias ea deleniti cum,
+          reprehenderit, itaque, officia necessitatibus aspernatur facilis
+          suscipit. Ducimus nemo voluptatum consequatur?
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Email address.. " />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>16,000+ People requested access in the last 24 hours.</p>
+        </div>
+      </div>
+
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai header" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
